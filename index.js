5b1707bc8fb5cadfd5c53957cc6bb9b7e2c3f1a6
samcomma <samcomerford1825@gmail.com>
2018-10-01 20:10:12 -0400

    all routes set up

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const volleyball = require('volleyball')
+app.use(require('body-parser').json())
 const db = require('./db')
 const { School, Student } = db.models
 
 const port = process.env.PORT || 3000 
 app.listen(port, ()=> console.log(`Now listening to port: ${port}`))
 
-
+app.use(volleyball)
 
 app.use('/dist', express.static(path.join(__dirname, 'dist')))
 
@@ -17,10 +19,13 @@ app.get('/', (req, res, next)=> {
 })
 
 
+// SCHOOL ROUTES:
 app.get('/api/schools', (req, res, next)=> {
   School.findAll({
-    where: req.query,
-    include: [ Student ]
+    include: [ Student ],
+    order: [
+    ['name', 'ASC']
+    ]
   })
     .then(students => res.json(students))
     .catch(next)
@@ -36,10 +41,37 @@ app.get('/api/schools/:schoolId', (req, res, next)=> {
 })
 
 
+app.post('/api/schools', (req, res, next)=> {
+  School.create(req.body)
+  .then(school => res.json(school))
+  .catch(next)
+})
+
+
+app.put('/api/schools/:id', (req, res, next)=> {
+  School.findById(req.params.id)
+  .then(school => school.update(req.body))
+  .then(school => res.json(school)) 
+  .catch(next)
+})
+  
+  
+app.delete('/api/schools/:id', (req, res, next)=> {
+  School.findById(req.params.id)
+  .then(school => school.destroy())
+  .then(() => res.sendStatus(204))
+  .catch(next)
+})
+  
+   
+
+// STUDENT ROUTES:
 app.get('/api/students', (req, res, next)=> {
   Student.findAll({
-    where: req.query,
-    include: [School]
+    include: [School],
+    order: [
+    ['firstName', 'ASC']
+    ]    
   })
     .then(students => res.json(students))
     .catch(next)
@@ -55,5 +87,39 @@ app.get('/api/students/:studentId', (req, res, next)=> {
 })
 
 
+app.post('/api/students', (req, res, next)=> {
+  Student.create(req.body)
+  .then(student => res.json(student)) 
+  .catch(next)
+})
+
+
+app.put('/api/students/:id', (req, res, next)=> {
+  Student.findById(req.params.id)
+  .then(student => student.update(req.body))
+  .then(student => res.json(student)) 
+  .catch(next)
+})
+
+
+app.delete('/api/students/:id', (req, res, next)=> {
+  Student.findById(req.params.id)
+  .then(student => student.destroy())
+  .then(() => res.sendStatus(204))
+  .catch(next)
+})
+
+
+
+// ERROR HANDLER
+app.use((err, req, res, next) => {
+  console.error(err, typeof next)
+  console.error(err.stack)
+  res.status(err.status || 500).send(err.message || 'Internal server error.')
+})
+  
+
+
+// SYNC AND SEED THE DATABASE
 db.syncAndSeed()
   .then(()=> console.log('Synced and Seeded'))
